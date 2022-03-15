8e506d71b811759b6e0eff8d3953a4e66cba8ca9
ghoudi01 <31049656+ghoudi01@users.noreply.github.com>
2022-03-15 15:21:23 +0100

    Update GradientSlider.js

--- src/sliders/GradientSlider.js
+++ src/sliders/GradientSlider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Platform } from 'react-native';
 import PropTypes from 'prop-types';
-import Slider from 'react-native-slider';
+import Slider from '@react-native-community/slider';
 import Gradient from '../gradients/Gradient';
 
 const GradientSlider = ({
